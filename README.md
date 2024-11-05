9f17dc4fe02ce46addb073c3d6eb606cbb34c723
Josh Custance <jcustance@spyglassmtg.com>
2024-11-05 11:13:24 -0500

    Update

--- README.md
+++ README.md
@@ -4,8 +4,6 @@ This extension will save a polyglot notebook (.dib) file as a text file so that 
 
 This is a bug introduced by the polyglot notebook developers that chose the .DIB extension which was already in use as a binary file, therefore it does not render in Azure DevOps as a text file and cannot/willnot show the changes.
 
-Example:
-    ```PolyglotNotebook.dib``` copied to: ```PolyglotNotebook.dib.txt``` on save.
-
-- Joshua Custance
-- (C) 2024 Spyglass MTG, LLC
+Joshua Custance
+Spyglass MTG
+(C) 2024
--- src/extension.ts
+++ src/extension.ts
@@ -3,18 +3,18 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.info('DIB2TXT Extension Active');
-    let disposable = vscode.workspace.onDidSaveNotebookDocument((document: vscode.NotebookDocument) => {
-        if (document.uri.fsPath.endsWith('.dib')) {
-			console.info('DIB2TXT: A DIB Document was saved');
+	console.info('My Extensioni is active')
+    let disposable = vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
+        if (document.fileName.endsWith('.dib')) {
+			console.info('A DIB Document was saved');
             const dibFilePath = document.uri.fsPath;
             const txtFilePath = dibFilePath.replace(/\.dib$/, '.dib.src');
             fs.copyFile(dibFilePath, txtFilePath, (err) => {
                 if (err) {
-					console.error('DIB2TXT: An error occured copying file');
+					console.error('An error occured copying file');
                     vscode.window.showErrorMessage(`Failed to save .txt file: ${err.message}`);
                 } else {
-					console.info('DIB2TXT: A copy of the .dib was saved as .txt');
+					console.info('A copy of the dib was saved as .txt');
                     vscode.window.showInformationMessage(`Saved .txt file: ${txtFilePath}`);
                 }
             });
